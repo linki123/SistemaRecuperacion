7e7158f5000060efd15eb0bad3567e2425baab20
linki123 <alvaroaries839@gmail.com>
2026-06-15 23:56:02 -0500

    Add files via upload

--- db.js
+++ db.js
@@ -1,18 +1,31 @@
+const mysql = require("mysql2/promise");
+
 const db = mysql.createPool({
 
     host:
-        process.env.DB_HOST,
+        process.env.MYSQLHOST || "localhost",
 
     user:
-        process.env.DB_USER,
+        process.env.MYSQLUSER || "root",
 
     password:
-        process.env.DB_PASSWORD,
+        process.env.MYSQLPASSWORD || "7301226517593",
 
     database:
-        process.env.DB_NAME,
+        process.env.MYSQLDATABASE || "sistema_recuperacion",
 
     port:
-        process.env.DB_PORT
+        process.env.MYSQLPORT || 3306,
+
+    waitForConnections:
+        true,
+
+    connectionLimit:
+        10,
+
+    queueLimit:
+        0
 
 });
+
+module.exports = db;
